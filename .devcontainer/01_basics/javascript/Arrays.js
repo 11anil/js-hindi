9d92afb6d8b686d99f0aaa153612456bb05eb961
Dheerajjha451 <dheerajjha451@gmail.com>
2023-04-03 20:09:35 +0530

    add

---
+++ .devcontainer/01_basics/javascript/Arrays.js
@@ -0,0 +1,11 @@
+// let numbers =[1,2,3,7]
+// console.log(numbers[2])
+// console.log(numbers.length)
+// let n= [1,7,9]
+// console.log(n.toString())
+// let a=n.join("-")
+// console.log(a)
+let a=[7,1,2,8]
+// let b=a.push(9)
+// console.log(b)
+console.log(a.shift())
