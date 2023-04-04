9364ef4ea0157ab66633723e28f6d9a8b1aede78
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-04-04 14:28:17 +0000

    variables in javascript

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 22233
+let accountemail = "hjjs@gmail.com"
+var accountpassword = "sdkms2w22"
+accountcity = "jaipur" 
+
+
+// accountId = 2 //not alllowed
+console.log(accountId)
+
+"use strict";
+console.log ("Anil");
+console.log (3+3);
