17b89d0d33c2653ef939b944536e56056fdc1502
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-04-05 11:17:52 +0000

    a little change

--- .devcontainer/01_basics/variables.js
+++ .devcontainer/01_basics/variables.js
@@ -7,6 +7,8 @@ accountcity = "jaipur"
 // accountId = 2 //not alllowed
 console.log(accountId)
 
-"use strict";
+"use strict";// use javascript code as newer version
+
+//alert(3+3) // we are using nodejs not browser
 console.log ("Anil");
 console.log (3+3);
