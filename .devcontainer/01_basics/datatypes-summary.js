5e4554573fdc12fbb2d68f3b322f4ff4c152a93e
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-04-10 02:14:40 +0000

    datatypes-summary.js

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,56 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// // const bigNumber = 3456543576654356754n
+
+
+
+// // Reference (Non primitive)
+
+// // Array, Objects, Functions
+
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "Anil",
+//     age: 20,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+
+// console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+let myyoutubename = "Anil";
+let anothername = myyoutubename
+anothername = "jaiho";
+
+console.log(myyoutubename);
+console.log(anothername);
+
+let userOne = {
+    email:  "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.emai = "anil@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
