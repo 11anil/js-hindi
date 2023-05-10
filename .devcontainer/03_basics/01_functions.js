ec50cbe5a76a84a02369eef8f2973a2a947f0eb2
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-10 04:16:26 +0000

    Function with objects and array in javascript

--- .devcontainer/03_basics/01_functions.js
+++ .devcontainer/03_basics/01_functions.js
@@ -43,5 +43,34 @@ function loginusermessage(username = "mohit"){
 }
 
 // console.log(loginusermessage("Anil"));
-console.log(loginusermessage("Anil"));
+// console.log(loginusermessage("Anil"));
 
+function calculatecartprice(val1,val2,...num1){  // in this case val1 and val2 will store the 200 & 400 value and the rest will give the  value in array form
+                                                        // ... (rest operator)
+     return num1
+}
+// console.log((calculatecartprice (200,400,800,2000)));
+
+const user = {
+    username : "Anil",
+    price : 199
+}
+
+function handleanyobject(anyobject){
+    console.log(`username is : ${anyobject.username} price is : ${anyobject.price}`);
+}
+
+// handleanyobject(user)
+
+// handleanyobject({
+//     username : "mohit",
+//     price : 39
+// })
+
+
+const mynewarray = [200 , 400 ,900 ,300]
+function returnsecondvalue(getarray){
+    return getarray[2]
+}
+// console.log(returnsecondvalue(mynewarray));
+console.log(returnsecondvalue([200 ,400 , 900 , 1000]));
