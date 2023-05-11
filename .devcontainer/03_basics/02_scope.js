5cfb9a6f15200782821226c714dc45f72b930469
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-11 11:23:00 +0000

    scope in javascript

--- .devcontainer/03_basics/02_scope.js
+++ .devcontainer/03_basics/02_scope.js
@@ -1,9 +1,9 @@
- let a = 300;
-if(true){
+let a = 300;
+if (true) {
     let a = 10;
     const b = 20;
     var c = 30;
-    console.log("Inner :" , a);
+    console.log("Inner :", a);
 }
 // console.log(a);
 // console.log(b);
