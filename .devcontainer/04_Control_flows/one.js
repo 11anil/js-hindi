284983f90900913c970575034a96389d44e506b1
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-17 07:34:06 +0000

    control flow in js

---
+++ .devcontainer/04_Control_flows/one.js
@@ -0,0 +1,49 @@
+//  const userloggedin = true;
+//  const temparature = 41
+ 
+//  if(temparature ==40 ){
+//     console.log("less than 50");
+//  }else{
+//     console.log("greator than 50");
+//  }
+//  console.log("Temaparature is greator than 50");
+
+//  const score = 200;
+//  if(score > 100){
+//     var power = "fly";
+//     console.log(`user power is : ${power}`);
+//  }
+//  console.log(`user power is : ${power}`);
+
+const balance = 1000;
+//  if(balance > 500) console.log("test 1") , console.log("test2");
+
+
+// if(balance < 500) {
+//     console.log("less than 500");
+// }
+
+// else if(balance < 750) {
+//     console.log("less than 750");
+// }
+// else if(balance < 900){
+//   console.log("less than 900");
+// }
+// else {
+//     console.log("less than 1200");
+// }
+
+const userloggedin = true;
+const dabitcard = true;
+const userloggedinfromgoogle = false;
+const userloggedinfromemail = true
+
+if(userloggedinfromemail || userloggedinfromgoogle){
+    console.log("user logged in");
+}
+
+
+
+// if(userloggedin && debitcard){
+//     console.log("Allow to buy courses");
+// }
