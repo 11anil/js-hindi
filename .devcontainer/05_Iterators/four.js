773e9c5416b96dbcd95b85685d93aff4bfa06893
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-26 11:54:41 +0000

    high order  array loops (forin)

---
+++ .devcontainer/05_Iterators/four.js
@@ -0,0 +1,36 @@
+const myobject = {
+    js : 'javascript',
+    cpp : 'c++',
+    rb : 'ruby',
+    swift : 'swift by apple'
+}
+
+//forin 
+
+//in this loop it will firstly generate keys not the values 
+
+for (const key in myobject) {
+//    console.log(`${key} shortcut is for ${myobject[key]}`);
+        
+    }
+
+    const programming = ['js','rb','py','java','cpp']
+
+    for (const key in programming) {
+    //    console.log(programming[key]);
+            
+        }
+
+//         Map = new Map()
+
+// Map.set('IN', 'INDIA')
+// Map.set('fr',"france")
+// Map.set('ch',"china")
+// Map.set('fr',"france")
+                                 // (// this action cannot be perform because map is not iterable)
+// for (const key in Map) {
+    
+//        console.log(key);  
+//     }
+
+    
