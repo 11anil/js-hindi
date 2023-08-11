1d5c3bc9983f88cdc02568ae1bdb146fca1f1ac2
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-08-11 17:34:56 +0000

    Async js que solution

---
+++ .devcontainer/08_Async/User.js
@@ -0,0 +1,22 @@
+//user will ask for a number
+// between 0-9 and if the Number
+// is below 5 resolve if not reject
+
+var ans = new Promise((res,rej)=>{
+    var n = Math.floor(Math.random()*10);
+
+    if(n < 5){
+        return res();
+    }
+    else{
+        return rej();
+    }
+
+})
+
+ans.then(function(){
+    console.log("below");
+})
+.catch(function(){
+    console.log("above");
+})
